c82cba8aebaaefa25c789264a4782b9a395c36a1
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2022-05-25 19:12:11 -0300

    Feat: Criando Api de produtos.

---
+++ GeekShopping.ProductAPI/Startup.cs
@@ -0,0 +1,32 @@
+﻿namespace GeekShopping.ProductAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        private IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+        }
+    }
+}
