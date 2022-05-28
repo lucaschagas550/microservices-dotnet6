e6d67439b7d5e36e517b38702f75561cc43e6199
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2022-05-28 15:21:47 -0300

    Feat: Definindo Operações da interface IProductServices.

---
+++ GeekShopping.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using GeekShopping.Web.Models;
+
+namespace GeekShopping.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductModel>> FindAllProduct();
+        Task<ProductModel> FindById(long id);
+        Task<ProductModel> CreateProduct(ProductModel product);
+        Task<ProductModel> UpdateProduct(ProductModel product);
+        Task<bool> DeleteProductById(long id);
+    }
+}
