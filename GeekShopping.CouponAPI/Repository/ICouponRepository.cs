488b8a52a9e60dc305639877ef857691d19ac622
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2022-06-10 19:02:28 -0300

    Feat: Implementando as classes do repositorio.

---
+++ GeekShopping.CouponAPI/Repository/ICouponRepository.cs
@@ -0,0 +1,9 @@
+﻿using GeekShopping.CouponAPI.Data.ValueObjects;
+
+namespace GeekShopping.CouponAPI.Repository
+{
+    public interface ICouponRepository
+    {
+        Task<CouponVo> GetCouponByCouponCode(string couponCode);
+    }
+}
