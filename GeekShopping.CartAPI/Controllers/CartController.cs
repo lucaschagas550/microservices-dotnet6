5309a4dfe036c51f3176b46414d7f05bb9dcdcc9
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2022-06-12 13:57:48 -0300

    Feat: implementing checkout

--- GeekShopping.CartAPI/Controllers/CartController.cs
+++ GeekShopping.CartAPI/Controllers/CartController.cs
@@ -67,5 +67,18 @@ namespace GeekShopping.CartAPI.Controllers
 
             return Ok(status);
         }
+
+        [HttpPost("checkout")]
+        public async Task<ActionResult<CheckoutHeaderVO>> Checkout(CheckoutHeaderVO vo)
+        {
+            var cart = await _repository.FindCartByUserId(vo.UserId);
+            if (cart == null) return NotFound();
+            vo.CartDetails = cart.CartDetails;
+            vo.DateTime = DateTime.Now;
+
+            //TASK RabbitMQ logic comes here!!!
+
+            return Ok(vo);
+        }
     }
 }
