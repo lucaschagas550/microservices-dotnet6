3bfce4656582f4d2f26080778142007051c15215
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2022-06-14 19:39:20 -0300

    Feat: Creating micro service geekShopping.Order

---
+++ GeekShopping.OrderAPI/Startup.cs
@@ -0,0 +1,34 @@
+﻿namespace GeekShopping.OrderAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        private IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+        }
+    }
+}
