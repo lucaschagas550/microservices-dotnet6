723bfea7be7b3d7c22e944417b4be02458a6f326
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2022-06-17 18:55:43 -0300

    Feat: Creating Api for Email.

---
+++ GeekShopping.Email/Startup.cs
@@ -0,0 +1,35 @@
+﻿namespace GeekShopping.Email
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        private IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllers();
+        }
+    }
+}
